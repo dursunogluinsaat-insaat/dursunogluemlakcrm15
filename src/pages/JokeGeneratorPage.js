409fff9006d729ca4f083a360032691c23741639
dursunogluinsaat-insaat <dursunogluinsaat@gmail.com>
2026-07-12 20:55:09 +0300

    Add JokeGeneratorPage

---
+++ src/pages/JokeGeneratorPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import JokeGenerator from '../components/JokeGenerator';
+
+function JokeGeneratorPage() {
+  return <JokeGenerator />;
+}
+
+export default JokeGeneratorPage;
